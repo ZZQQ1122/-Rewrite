00aceb7996d7417228cdc8dfc6aa2f2bc0161aa5
ZZQQ1122 <126233867+ZZQQ1122@users.noreply.github.com>
2026-07-13 14:39:44 +0800

    Update script URLs for revenuecat API

--- yosum.vip.js
+++ yosum.vip.js
@@ -7,8 +7,8 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
-^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Reheji.js
+^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/ZZQQ1122/-Rewrite/refs/heads/master/yosum.vip.js
+^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/ZZQQ1122/-Rewrite/refs/heads/master/yosum.vip.js
 
 [mitm]
 hostname = api.revenuecat.com, api.rc-backup.com
